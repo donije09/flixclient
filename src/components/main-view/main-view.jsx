932959291abb3ec8f106c84669b1d019b83ad138
donije09 <ustinedon@gmail.com>
2024-08-02 17:08:37 +0200

    Add login and signup views

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,56 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import axios from'axios';
-import MovieCard from '../movie-card/movie-card';
-import MovieView from '../movie-view/movie-view';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import { Container, Row, Col, Button, Form, Card } from 'react-bootstrap';
+import { MovieCard } from './movie-card/movie-card';
+import { LoginView } from './login-view/login-view';
+import { SignupView } from './signup-view/signup-view';
 
-
-/*import inceptionPoster from '../../images/inception.jpg';
-import interstellarPoster from '../../images/interstellar.jpg';
-import darkKnightPoster from '../../images/dark-knight.jpg';*/
-
-const MainView = () => {
+export const MainView = () => {
   const [movies, setMovies] = useState([]);
+  const [user, setUser] = useState(null);
+  const [isSignup, setIsSignup] = useState(false);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
-useEffect(() => {
-  const token = localStorage.getItem('token');
-  axios.get(' https://glacial-retreat-35130-2f56298b8e37.herokuapp.com/movies', {
-    headers: { Authorization: `Bearer ${token}`}
-  })
-    .then(response => {
-      setMovies(response.data);
-    })
-    .catch(error => {
-      console.error('There was an error fetching the movies!', error);
-    });
-}, []);
+  useEffect(() => {
+    if (user) {
+      axios.get('https://glacial-retreat-35130-2f56298b8e37.herokuapp.com/movies', {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+      })
+      .then(response => setMovies(response.data))
+      .catch(error => console.error('Error fetching movies:', error));
+    }
+  }, [user]);
+
+  const handleLogin = (username, password) => {
+    axios.post('https://glacial-retreat-35130-2f56298b8e37.herokuapp.com/login', { username, password })
+      .then(response => {
+        setUser(response.data.user);
+        localStorage.setItem('token', response.data.token);
+      })
+      .catch(error => console.error('Login error:', error));
+  };
+
+  const handleSignup = (user) => {
+    axios.post('https://glacial-retreat-35130-2f56298b8e37.herokuapp.com/users', user)
+      .then(response => {
+        handleLogin(user.username, user.password);
+      })
+      .catch(error => console.error('Signup error:', error));
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+  };
+
+  if (!user) {
+    return (
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md={8}>
+            {isSignup ? (
+              <SignupView onSignup={handleSignup} onSwitchToLogin={() => setIsSignup(false)} />
+            ) : (
+              <LoginView onLogin={handleLogin} onSwitchToSignup={() => setIsSignup(true)} />
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 
   return (
-    <div>
-      {selectedMovie ? (
-        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-      ) : (
-        movies.map(movie => (
-          <MovieCard key={movie.id} movie={movie} onMovieClick={(newSelectedMovie) => setSelectedMovie(newSelectedMovie)} />
-        ))
-      )}
-    </div>
+    <Container>
+      <Row className="mb-4">
+        <Col>
+          <Button variant="primary" onClick={handleLogout}>Logout</Button>
+        </Col>
+      </Row>
+      <Row>
+        {movies.map(movie => (
+          <Col key={movie._id} md={4} className="mb-4">
+            <MovieCard movie={movie} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
-// MainView component here
-
-MainView.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-    director: PropTypes.string.isRequired,
-    poster: PropTypes.string,
-  })),
-  selectedMovie: PropTypes.object,
-  setSelectedMovie: PropTypes.func,
-};
-
-export default MainView;
